b2883653b2c105d9d017f16b9baa87eeadc4219e
ehl <ewno92@gmail.com>
2025-02-25 00:05:34 -0800

    feat/http-test

--- lib/pages/test_page2.dart
+++ lib/pages/test_page2.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:study_flutter/components/my_button.dart';
 import 'package:study_flutter/theme/theme_provider.dart';
 
 class TestPage2 extends StatefulWidget {
@@ -10,6 +14,28 @@ class TestPage2 extends StatefulWidget {
 }
 
 class _TestPage2State extends State<TestPage2> {
+  Future<void> getTest() async {
+    final url = Uri.parse(
+      'http://localhost:3000/api/test/67bc14b100103d1475d4',
+    );
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        // Request successful
+        var jsonData = jsonDecode(response.body);
+        print(jsonData); // Print the received data
+        // Process the data
+      } else {
+        // Request failed
+        print('Request failed with status: ${response.statusCode}.');
+      }
+    } catch (e) {
+      // Handle network errors or other exceptions
+      print('Error: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -28,6 +54,7 @@ class _TestPage2State extends State<TestPage2> {
           },
           child: Center(child: Text("change theme color")),
         ),
+        MyButton(onTap: getTest, child: Container(child: Text("getHttp"))),
       ],
     );
   }
