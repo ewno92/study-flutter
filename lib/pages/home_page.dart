e04172530c29db37001cc516bf09f97d272ae088
ehl <ewno92@gmail.com>
2025-02-24 17:55:08 -0800

    feat/sidebar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -31,6 +31,63 @@ class _HomePageState extends State<HomePage> {
       bottomNavigationBar: BottomNavbar(
         onTabChange: (index) => navigateBottomNavbar(index),
       ),
+      appBar: AppBar(
+        leading: Builder(
+          builder:
+              (context) => IconButton(
+                icon: Icon(Icons.menu, color: Colors.black),
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+              ),
+        ),
+        title: const Text("appBar"),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child: Column(
+          children: [
+            // logo
+            DrawerHeader(child: Image.asset("lib/images/logo.png")),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Divider(color: Colors.grey[800]),
+            ),
+
+            // pages
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: ListTile(
+                leading: const Icon(Icons.home, color: Colors.white),
+                title: const Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                  navigateBottomNavbar(0);
+                },
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.only(left: 25),
+              child: ListTile(
+                leading: const Icon(Icons.home, color: Colors.white),
+                title: const Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onTap: () {
+                  Navigator.pop(context);
+                  navigateBottomNavbar(1);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
       body: _pages[_selectedIndex],
     );
   }
